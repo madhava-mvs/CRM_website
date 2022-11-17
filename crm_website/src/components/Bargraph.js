af099dbd7a8305fd2b3975c2b3756740e4c8bd03
Mrudul-Airpula <113907072+Mrudul-Airpula@users.noreply.github.com>
2022-11-17 08:44:40 +0530

    commit

--- crm_website/src/components/Bargraph.js
+++ crm_website/src/components/Bargraph.js
@@ -36,14 +36,45 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 // }
 
 
-export default function Bargraph({ bar }) {
+export default function Bargraph({ bar, campaignstatus, bargraph_status, filtercount, count_value, campaignwiseprospect_popup_show }) {
+
 
     return <>
         <div className="bargraph">
             <div className="bargraph_title">
                 <div >
-                    <label >Campaign wise Prospect Count </label>
-                    <BsThreeDotsVertical className="icon" />
+                    <div className="bargraph_title_row">
+                        <div className="bargraph_title_row_title">
+                            <label >Campaign wise Prospect Count </label>
+                        </div>
+                        <div className="threedot_icon_div">
+                            <BsThreeDotsVertical className="bargraph_threedot_icon" />
+                        </div>
+                    </div>
+                    {campaignwiseprospect_popup_show ?
+                        (<div className="bargraph_campaignwiseprospect_popup">
+                            <div className="bargraph_campaignwiseprospect_popup_content">
+                                <label>Status</label>
+                                <select className="campaign_rectangle_select" value={bargraph_status} onChange={(e) => campaignstatus(e)} >
+                                    <option value="empty">Select</option>
+                                    <option value="In Progress" onClick={campaignstatus}>In Progress</option>
+                                    <option value="To Do" onClick={campaignstatus}>To Do</option>
+                                    <option value="Completed" onClick={campaignstatus} >Completed</option>
+                                </select>
+
+                                <label>Limit</label>
+                                <select className="campaign_rectangle_select" value={count_value} onChange={(e) => filtercount(e)} >
+                                    <option value="empty">Select</option>
+                                    <option value="1" onClick={filtercount}>1</option>
+                                    <option value="2" onClick={filtercount}>2</option>
+                                    <option value="3" onClick={filtercount}>3</option>
+                                    <option value="4" onClick={filtercount}>4</option>
+                                    <option value="5" onClick={filtercount}>5</option>
+                                    {/* <option value="">--6-</option> */}
+                                </select>
+                            </div>
+                        </div>) : (<></>)
+                    }
                 </div>
             </div>
             <div className="bargraph_graph">
--- crm_website/src/components/Horizontalbar.js
+++ crm_website/src/components/Horizontalbar.js
@@ -2,16 +2,38 @@ import React from "react";
 import { FaDollarSign } from "react-icons/fa";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import "./horizontalbar.css"
-export default function Horizontalbar({ orangebar, greenbar, bluebar, show2, Horizontal_value, Horizontal_percentage }) {
+export default function Horizontalbar({ orangebar, greenbar, bluebar, show2, Horizontal_value, Horizontal_percentage, leadsfunnel_popup_show }) {
     return (
         <>
             <div className="Horizontalbar_main">
                 <div className="Horizontalbar_main_row1">
                     <label>Leads Funnel</label>
                     <div className="Horizontalbar_main_row1_icon">
-                        <BsThreeDotsVertical />
+                        <BsThreeDotsVertical className="threedots_icon" />
                     </div>
                 </div>
+                {leadsfunnel_popup_show ?
+                    (<div className="leadsfunnel_popup">
+                        <div className="leadsfunnel_popup_content">
+                            <label>Manager</label>
+                            <select className="leadsfunnel_popup_rectangle_select" >
+                                <option value="empty">Select</option>
+                                {/* <option value="In Progress" >In Progress</option>
+                                <option value="To Do" >To Do</option>
+                                <option value="Completed"  >Completed</option> */}
+                            </select>
+                            <label>Campaign</label>
+                            <select className="leadsfunnel_popup_rectangle_select" >
+                                <option value="empty">Select</option>
+                                {/* <option value="1" >1</option>
+                                <option value="2" >2</option>
+                                <option value="3" >3</option>
+                                <option value="4" >4</option>
+                                <option value="5" >5</option> */}
+                            </select>
+                        </div>
+                    </div>) : (<></>)
+                }
                 {show2 ? <div className="Horizontalbar_main_row2">
                     <div className="Horizontalbar_main_row2_left">
                         <FaDollarSign />
