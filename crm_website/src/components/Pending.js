c4217cf174caf279690607b3ef08ec360d2606c2
Mrudul-Airpula <113907072+Mrudul-Airpula@users.noreply.github.com>
2022-11-21 09:22:33 +0530

    commit

--- crm_website/src/components/Pending.js
+++ crm_website/src/components/Pending.js
@@ -9,8 +9,6 @@ export default function Pending({pending_show, setPending_show, approval_Func}) 
                     <label>Approve User</label>
                 </div>
                 <div className='pending_inner_row2'>
-                </div>
-                <div className='pending_inner_row3'>
                     <button onClick={approval_Func} >APPROVE</button>
                     <button onClick={(e)=>{setPending_show(!pending_show)}} >DISAPPROVE</button>
                 </div>
